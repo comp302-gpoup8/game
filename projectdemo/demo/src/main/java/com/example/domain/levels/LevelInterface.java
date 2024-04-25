5dcb0fb71f011b9be66f5f5375677e5e26a43161
BYBakay <bbakay18@ku.edu.tr>
2024-04-25 18:33:10 +0300

    Breaking down the original gamePanel class

--- projectdemo/demo/src/main/java/com/example/domain/levels/LevelInterface.java
+++ projectdemo/demo/src/main/java/com/example/domain/levels/LevelInterface.java
@@ -1,5 +1,7 @@
 package com.example.domain.levels;
 
+import java.awt.Point;
+
 import com.example.domain.barriers.Barrier;
 
 public interface LevelInterface extends LevelRandomizer {
@@ -9,6 +11,21 @@ public interface LevelInterface extends LevelRandomizer {
 
     public static void removeBarrier(Level l, Barrier b){
         l.barriers.remove(b);
+    }
+
+    public static void placeBarriers(Level level){
+        Point p = new Point(10, 0);
+        for (Barrier barrier : level.barriers){
+            if (outOfRow(p.x, barrier.getWidth())){
+                p = new Point(10, p.y + barrier.getHeight());
+            }
+            barrier.setLocation(p);
+            p.x += 5;
+        }
+    }
+
+    private static boolean outOfRow(int x, int w){
+        return x + w > 1200;
     }
 
     public static boolean cleared(Level l){
---
+++ projectdemo/demo/src/main/java/com/example/visual/BackgroundLabel.java
@@ -0,0 +1,14 @@
+package com.example.visual;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class BackgroundLabel extends JLabel {
+
+    protected ImageIcon backgroundImage;
+    protected String DEFAULT_IMG_PATH = "projectdemo/demo/src/main/java/com/example/Graphical-Assets/200Background.png";
+
+    public BackgroundLabel(){
+        backgroundImage = new ImageIcon(DEFAULT_IMG_PATH);
+    }
+}
