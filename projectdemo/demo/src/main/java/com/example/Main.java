8a94b0ecf963ca93b65fd3de000ec2a4c06cd6da
BYBakay <bbakay18@ku.edu.tr>
2024-04-26 09:43:42 +0300

    Can't see the level :/

--- projectdemo/demo/src/main/java/com/example/Main.java
+++ projectdemo/demo/src/main/java/com/example/Main.java
@@ -1,15 +1,12 @@
 package com.example;
 
-import java.util.Scanner;
+import com.example.domain.levels.Level;
+import com.example.visual.GamePanel;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        sc.close();
-        switch (choice) {
-            case 1 -> System.out.println("Hello World");
-            default -> System.out.println("Heya!");
-        }
+        Level level = new Level("RandomLevel");
+        GamePanel gp = new GamePanel(level);
+        
     }
 }
--- projectdemo/demo/src/main/java/com/example/visual/GamePanel.java
+++ projectdemo/demo/src/main/java/com/example/visual/GamePanel.java
@@ -1,23 +1,56 @@
 package com.example.visual;
 
-import com.example.domain.barriers.*;
-import com.example.domain.elements.*;
-import com.example.domain.levels.*;
+import java.awt.Dimension;
+import java.awt.Point;
 import javax.swing.JFrame;
+
+import com.example.domain.elements.Staff;
+import com.example.domain.levels.Level;
+import com.example.domain.levels.LevelInterface;
 
 public class GamePanel extends JFrame{
     
-    Level level;
-    FireBall fireBall;
-    Staff staff;
     BackgroundLabel bLabel;
     Controller controller;
+    Staff staff;
 
-    public GamePanel(Level l, FireBall f, Staff s){
-        level = l;
-        fireBall = f;
-        staff = s;
-        bLabel = new BackgroundLabel();
+    public GamePanel(Level level){
+        initGameFrame();
         controller = new Controller();
+        addKeyListener(controller.keyListener);
+        addLevel(level);
+        displayGamePanel();
     }
+
+    private void initGameFrame(){
+        setSize(1210, 680);
+        setTitle("Single Player");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        bLabel = new BackgroundLabel();
+        bLabel.setBounds(0, 0, getWidth(), getHeight());
+        add(bLabel);
+        bLabel.setVisible(true);
+
+    }
+
+    private void displayGamePanel(){
+        staff.setVisible(true);
+        // revalidate();
+        // repaint();
+        setVisible(true);
+        setFocusable(true);
+    }
+
+    public void addLevel(Level level){
+        // getContentPane().removeAll();
+        LevelInterface.placeBarriers(level);
+        level.barriers.forEach(e -> add(e));
+        Point staffPosition = new Point(getWidth() / 2, getHeight() - 100);
+        Dimension staffSize = new Dimension(120, 20);
+        staff = new Staff(staffPosition, staffSize);
+    }
+
+
+
 }
