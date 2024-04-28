7f0882ebed644f39cad27889492c1b5d6c7d5011
BYBakay <bbakay18@ku.edu.tr>
2024-04-28 16:21:21 +0300

    Added javadocs for the interfaces

--- projectdemo/demo/src/main/java/com/example/domain/interfaces/BallManager.java
+++ projectdemo/demo/src/main/java/com/example/domain/interfaces/BallManager.java
@@ -4,15 +4,30 @@ import java.awt.Point;
 import com.example.domain.gameObject.FireBall;
 import com.example.domain.gameObject.Staff;
 
+/**
+ * BallManager Interface
+ * Responsible for the movement of the FireBall, and it's re-initiation when it's lost.
+ */
 public interface BallManager {
 
+    /**
+     * Places the FireBall at the appropriate location relative to the staff.
+     * @param ball
+     * @param s
+     */
     public static void placeBallAtStaff(FireBall ball, Staff s){
         int staffCenterX = s.getX() + s.getWidth() / 2;
         int staffCenterY = s.getY() + s.getHeight() / 2;
-        Point p = new Point(staffCenterX - 10, staffCenterY - 30); //+5 so it doesn't get in the staff.
+        Point p = new Point(staffCenterX - 10, staffCenterY - 30); //Additional Values to prevent the ball from being placed awkwardly.
         resetBall(ball, p);
     }
 
+    /**
+     * Resets the values associated with the FireBall
+     * Used for re-initating the ball when the possesion is lost.
+     * @param ball
+     * @param p
+     */
     public static void resetBall(FireBall ball, Point p){
         ball.setSpeed(0);
         ball.setLocation(p);
@@ -20,12 +35,22 @@ public interface BallManager {
         ball.setDirection(new Point(-1, -1));
     }
 
+    /**
+     * Used to launch the FireBall from the Staff at the beginning of each try.
+     * @param ball
+     * @param p
+     */
     public static void launchFireBall(FireBall ball, Point p){
         ball.setDirection(p);
         ball.setSpeed(4);
         moveFireBall(ball);
     }
 
+    /**
+     * Used to calculate the next point that the FireBall will move towards while it is in motion.
+     * @param ball
+     * @return
+     */
     public static Point nextPoint(FireBall ball){
         Point ballDirection = validateBallDirection(ball);
         int x = ball.getX() + (ballDirection.x * ball.getSpeed());
@@ -33,6 +58,12 @@ public interface BallManager {
         return new Point(x, y);
     }
 
+    /**
+     * Used to deal with an issue which sometimes causes the direction of the FireBall to be null.
+     * May no longer be necessary but let's keep it for now just in case.
+     * @param ball
+     * @return
+     */
     public static Point validateBallDirection(FireBall ball){
         Point direction = ball.getDirection();
         if (direction == null){
@@ -42,6 +73,10 @@ public interface BallManager {
         return direction;
     }
 
+    /**
+     * Used to move the FireBall and it's hitbox to the next point.
+     * @param ball
+     */
     public static void moveFireBall(FireBall ball){
         Point p = nextPoint(ball);
         ball.setLocation(p);
--- projectdemo/demo/src/main/java/com/example/domain/interfaces/PhysicsManager.java
+++ projectdemo/demo/src/main/java/com/example/domain/interfaces/PhysicsManager.java
@@ -4,8 +4,22 @@ import java.awt.Point;
 import com.example.domain.gameObject.FireBall;
 import com.example.domain.gameObject.GameObject;
 
+/**
+ * Physics Manager Class
+ * Handles the bounce operation of the fireball as well as makes sure that it stays within
+ * the boundaries of the level.
+ */
 public interface PhysicsManager {
 
+
+    /**
+     * Returns true if the FireBall comes in contact with the boundries of the level and makes it bounce off of them.
+     * Returns false if it reaches to the bottom of the level which causes the player to lose a life.
+     * @param ball
+     * @param screenHeight
+     * @param screenWidth
+     * @return
+     */
     public static boolean checkBounds(FireBall ball, int screenHeight, int screenWidth){
         int x = ball.getX();
         int y = ball.getY();
@@ -26,18 +40,27 @@ public interface PhysicsManager {
             return true;
         }
     }
-    
+    /**
+     * Bounces the FireBall off of an object, based on the point of contact.
+     * @param ball
+     * @param e
+     */
     public static void bounce(FireBall ball, GameObject e) {
         Point ballCenter = getObjectCenter(ball);
-        Point staffCenter = getObjectCenter(e);
+        Point objectCenter = getObjectCenter(e);
 
-        int hitPoint = ballCenter.x - staffCenter.x;
+        int hitPoint = ballCenter.x - objectCenter.x;
         ball.getDirection().x += 2 * hitPoint / e.getWidth();
         ball.getDirection().y *= -1;
         BallManager.moveFireBall(ball);
     }
 
-
+    /**
+     * Gets the center point of a gameObject.
+     * Used for the computation of the Bounce Method.
+     * @param e
+     * @return
+     */
     private static Point getObjectCenter(GameObject e){
         int centerX = e.getX() + e.getWidth() / 2;
         int centerY = e.getY() + e.getHeight() / 2;
