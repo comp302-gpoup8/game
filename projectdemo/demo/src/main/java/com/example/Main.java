e4abfb2ba3fdc92a248069a83dc392cb67223294
olordevilg <msanim21@ku.edu.tr>
2024-04-29 01:27:27 +0300

    Hearts fixed + documentation.

--- projectdemo/demo/src/main/java/com/example/Main.java
+++ projectdemo/demo/src/main/java/com/example/Main.java
@@ -2,24 +2,27 @@ package com.example;
 
 import com.example.domain.levels.Level;
 import com.example.domain.levels.LevelBuilder;
+import com.example.visual.LoginPanel;
 import com.example.visual.MainMenuPanel;
 import com.example.domain.Game;
 
 public class Main {
 
     public static void main(String[] args) {
-        MainMenuPanel mainMenu = new MainMenuPanel();
-        while (mainMenu.getGame() == null){
-            mainMenu.run();
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e){
-                e.printStackTrace();
-            }
-        }
-        Game game = mainMenu.getGame();
-        mainMenu = null;
-        game.run();
+//        MainMenuPanel mainMenu = new MainMenuPanel();
+//        while (mainMenu.getGame() == null){
+//            mainMenu.run();
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//        }
+//        Game game = mainMenu.getGame();
+//        mainMenu = null;
+//        game.run();
+        LoginPanel mainMenu = new LoginPanel();
+        mainMenu.run();
     }
 
 
