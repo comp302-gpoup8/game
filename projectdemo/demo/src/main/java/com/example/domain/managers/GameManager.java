173bdfdf316331045bd1dda08210f2e3927d1e3c
BYBakay <bbakay18@ku.edu.tr>
2024-04-28 16:33:22 +0300

    Added javadocs for GameManager

--- projectdemo/demo/src/main/java/com/example/domain/managers/GameManager.java
+++ projectdemo/demo/src/main/java/com/example/domain/managers/GameManager.java
@@ -13,8 +13,12 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
+/**
+ * GameManager class
+ * Allows interaction with all the manager interfaces.
+ * Contains the main game logic.
+ * TODO: Used to be too long and now partitioned, so definitely needs to be cleaned up but let's not break anything today.
+ */
 public class GameManager implements BallManager, CollisionHandler, PhysicsManager, StaffManager {
     private Game game;
     private List<GameObject> elements;
@@ -27,34 +31,61 @@ public class GameManager implements BallManager, CollisionHandler, PhysicsManage
         ball = game.getPanel().getBall();
     }
 
+    /**
+     * Allows the BallManager to launch the Ball.
+     * TODO: The ball should be launched in the Mouse Direction. Right now when I try it, it results in a null pointer exception.
+     */
     public void launchBall(){
         BallManager.launchFireBall(ball, new Point(-1, -1));
     }
 
+    /**
+     * Allows the Ballmanager to move the ball.
+     * TODO: All panel updates contain random checkBounds and checkCollision calls
+     * to prevent the ball from going through multiple barriers in a second.
+     * TODO: We should figure out the appropriate placement.
+     */
     public void moveBall(){
         updateElements();
         checkBounds();
         BallManager.moveFireBall(ball);
     }
 
+    /**
+     * Allows the StaffManager to move the Staff.
+     * @param s
+     * @param x
+     */
     public void moveStaff(Staff s, int x) {
         StaffManager.moveStaff(s,x, game.getPanel().getWidth());
         moveBallBeforeLaunch();
         game.getPanel().refreshLevel();
     }
 
+    /**
+     * Allows the FireBall to move along with the staff before it is launched.
+     */
     private void moveBallBeforeLaunch(){
         if (ball. getSpeed() == 0){
             reinitiateBall(ball);
         }
     }
     
+    /**
+     * Updates the panel GUI.
+     * TODO: All panel updates contain random checkBounds and checkCollision calls to prevent the ball from going through multiple barriers in a second.
+     * TODO: We should figure out the appropriate placement.
+     */
     public void updateElements() {
         PhysicsManager.checkBounds(ball, game.getPanel().getHeight(), game.getPanel().getWidth());
         game.getPanel().refreshLevel();
 
     }
 
+    /**
+     * Calls the PhysicsManager to check if the FireBall is within appropraite bounds.
+     * Reduces the player's life points when the ball is lost, and reinitates it.
+     */
     public void checkBounds() {
         boolean inGame = PhysicsManager.checkBounds(ball, game.getPanel().getHeight(), game.getPanel().getWidth());
         if (!inGame){
@@ -63,10 +94,17 @@ public class GameManager implements BallManager, CollisionHandler, PhysicsManage
         }
     }
 
+    /**
+     * Calls the BallManager to reset the Ball.
+     * @param f
+     */
     private void reinitiateBall(FireBall f) {
         BallManager.placeBallAtStaff(ball, game.getPanel().getStaff());
     }
 
+    /**
+     * Uses the CollisionHandler to check for collisions and removes destroyed objects as a result, when appropriate.
+     */
     public void checkCollisions() {
         List<GameObject> toRemove = new ArrayList<>();
 
